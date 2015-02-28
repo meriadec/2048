e2ebe1ceb8f3bdd7aea136558a46421a1e82dfb4
Balthazar Gronon <bgronon@gmail.com>
2015-02-28 12:01:53 +0100

    chore(draw): example key

--- src/main.c
+++ src/main.c
@@ -6,18 +6,19 @@
 /*   By: bgronon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/28 10:21:00 by bgronon           #+#    #+#             */
-/*   Updated: 2015/02/28 11:19:28 by bgronon          ###   ########.fr       */
+/*   Updated: 2015/02/28 12:01:24 by bgronon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game_2048.h"
-#include <ncurses.h>
 
 int main(void)
 {
 	if (!initscr()) {
 		ft_putendl("Init failed");
 	}
+
+	keypad(stdscr, TRUE);
 
 	int key;
 	while ((key = getch()) != 27) {
@@ -26,6 +27,11 @@ int main(void)
 			mvprintw(0, 0, "Col %d Line %d", COLS, LINES);
 			refresh();
 		}
+		if (key == KEY_UP) {
+			mvprintw(0, 0, "PUTE");
+			refresh();
+		}
+
 	}
 
 	endwin();
